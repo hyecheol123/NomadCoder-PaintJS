5d735380c3a5873216ca2856f8cbedb6a7044ebb
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-11-07 08:57:29 +0900

    Filling Canvas

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ Only used HTML, CSS, and Vanilla JavaScript.
 - Drawing on the canvas
   - Handle mouse events
   - Change Color and Thickness of brush
+  - Fill/Paint on the canvas
 - Design for Desktop site (CSS)
 
 **What I added/modified**
--- painting-board/app.js
+++ painting-board/app.js
@@ -2,6 +2,7 @@
 const canvas = document.getElementById('canvas');
 const colorControls = document.getElementsByClassName('color-control');
 const thicknessControl = document.getElementById('thickness');
+const modeBtn = document.getElementById('mode');
 // Setup Canvas
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -9,11 +10,13 @@ canvas.height = canvas.offsetHeight;
 // Generate the drawing context of the canvas
 const context = canvas.getContext('2d');
 context.strokeStyle = '#2c2c2c'; // Set the line color to black by default
+context.fillStyle = 'white'; // Set the fill color to white by default
 context.lineWidth = 2.5; // Set the line width as 2.5 by default
 context.lineCap = 'round'; // Set the line ending shape
 
 // Global variable
 let painting = false; // whether the user is on painting mode or not
+let filling = false; // By default, the brush will paint on the canvas
 
 // Change color
 for (colorBtn of colorControls) {
@@ -27,12 +30,32 @@ thicknessControl.addEventListener('input', () => {
   context.lineWidth = thicknessControl.value;
 });
 
+// Change Mode (Paint <-> Fill)
+modeBtn.addEventListener('click', () => {
+  if(filling) {
+    filling = false;
+    modeBtn.innerText = 'Fill';
+  } else {
+    filling = true;
+    modeBtn.innerText = 'Paint';
+  }
+});
+
 // Canvas EventListeners for Mouse Events
+// Click on canvas to fill the canvas
+canvas.addEventListener('click', () => {
+  if(filling) {
+    // Set the color
+    context.fillStyle = context.strokeStyle;
+    // fill the canvas
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+});
 // Draw when mouse moves
 canvas.addEventListener('mousemove', (mouseEvent) => {
   const cordX = mouseEvent.offsetX;
   const cordY = mouseEvent.offsetY;
-
+  
   // Paint a line (or point)
   if(!painting) {
     context.beginPath();
